d44e06ea80cbe23bf1be0d6af5b2d0f06ffae57d
priyank viradiya <priyankviradiya227@gmail.com>
2025-03-22 00:07:08 +0530

    Auction Item schema, controller and routes are successfully done

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ import fileUpload from "express-fileupload";
 import {connection} from './database/connection.js';
 import { errorMiddleware } from './middlewares/error.js';
 import userRouter from './router/userRoutes.js';
+import auctionItemRouter from "./router/auctionItemRoutes.js";
 
 
 const app = express();
@@ -32,6 +33,8 @@ app.use(
   );
 
 app.use('/api/v1/user', userRouter);
+app.use("/api/v1/auctionitem", auctionItemRouter);
+
 connection();
 app.use(errorMiddleware)
 export default app;
