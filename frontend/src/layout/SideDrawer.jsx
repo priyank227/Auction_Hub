56df7dec67cc37434dc1a24bd00da0dead39a3a1
mihir rupapara <mihirrupapara07@gmail.com>
2025-03-29 10:08:05 +0530

    update

--- frontend/src/layout/SideDrawer.jsx
+++ frontend/src/layout/SideDrawer.jsx
@@ -13,38 +13,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../store/slices/userSlice";
 import { Link } from "react-router-dom";
 
-// const SideDrawer = () => {
-//   const [show, setShow] = useState(false);
-//   const { isAuthenticated, user } = useSelector((state) => state.user);
-
-//   const dispatch = useDispatch();
-//   const handleLogout = () => {
-//     dispatch(logout());
-//   };
-
-//   return (
-//   <>
-//     <div
-//         onClick={() => setShow(!show)}
-//         className="fixed right-5 top-5 bg-[#D6482B] text-white text-3xl p-2 rounded-md hover:bg-[#b8381e] lg:hidden"
-//       >
-//         <GiHamburgerMenu />
-        
-//       </div>
-//       <div
-//         className={`w-[100%] sm:w-[300px] bg-[#f6f4f0] h-full fixed top-0 ${
-//           show ? "left-0" : "left-[-100%]"
-//         } transition-all duration-100 p-4 flex flex-col justify-between lg:left-0 border-r-[1px] border-r-stone-500`}
-//       ></div>
-//   </>
-//   );
-// };
-
-// export default SideDrawer;
-
-
-
-
 const SideDrawer = () => {
   const [show, setShow] = useState(false);
 
